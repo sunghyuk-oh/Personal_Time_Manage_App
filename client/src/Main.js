7c637e4c02138ab7a7ba8a1ba14981ef01ae53eb
Tim Oh <dhquffl@gmail.com>
2021-10-01 11:23:08 -0400

    Procfile added

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom'
+import logo from './logo2.png'
 
 export function Menu() {
     return (
         <nav className='navbar'>
             <ul>
                 <li><NavLink to='/'>Home</NavLink></li>
-                <li></li>
+                {/* <li><img src={logo} /></li> */}
                 <li><NavLink to='/history'>History</NavLink></li>    
             </ul>
         </nav>
